fab05f06a56c53bad06009b50aa913bc421277ed
Christos Charalampidis <Chrigeta@hotmail.com>
2021-11-10 11:17:53 +0200

    initial of solution structure.

---
+++ Viber.Net.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Viber.Net.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
